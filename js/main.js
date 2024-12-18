c50f050269d90e8d1deae1458193f3429f18eb2d
lakshadeep91 <lakshadeep91@gmail.com>
2024-12-17 20:59:55 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -746,6 +746,11 @@
 			$('.modal .modal-footer .btn-success').attr('href', url);
 		});
 
-
-
+		// Down Arrow Scrollable on Arrangements page
+		$(function() {
+			$('a[href*=#]').on('click', function(e) {
+				e.preventDefault();
+				$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+			});
+		});
 }());
